f7ceae7b8ca8875ddb56c8d27e873d0157394843
Baptiste Leduc <baptiste.leduc@gmail.com>
2020-12-08 21:28:26 +0100

    With next branch

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/dto.php';
 
-use Jane\AutoMapper\AutoMapper;
+use Jane\Component\AutoMapper\AutoMapper;
 use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
 use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
 use Doctrine\Common\Annotations\AnnotationReader;
-use Jane\AutoMapper\Loader\FileLoader;
-use Jane\AutoMapper\Generator\Generator;
+use Jane\Component\AutoMapper\Loader\FileLoader;
+use Jane\Component\AutoMapper\Generator\Generator;
 use PhpParser\ParserFactory;
 use Symfony\Component\Serializer\Mapping\ClassDiscriminatorFromClassMetadata;
 
